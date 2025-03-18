ce8d6896fc4cb1e38c9f96a06b70a947b77dbcbe
JoshuaWard9080 <Joshuaward9080@gmail.com>
2025-03-18 08:50:15 -0700

    Added Personal Scene and Header Fixes
    Added a personal scene for my testing and use. Fixed the headers and the variables so that they properly show up in the Inspector

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -5,14 +5,14 @@ using UnityEngine.Events;
 public class InputManager : MonoBehaviour
 {
     [Header("Player One Keybinds")]
-    private KeyCode playerOneLeft = KeyCode.A;
-    private KeyCode playerOneRight = KeyCode.D;
-    private KeyCode playerOneJump = KeyCode.A;
+    [SerializeField] private KeyCode playerOneLeft = KeyCode.A;
+    [SerializeField] private KeyCode playerOneRight = KeyCode.D;
+    [SerializeField] private KeyCode playerOneJump = KeyCode.W;
 
     [Header("Player Two Keybinds")]
-    private KeyCode playerTwoLeft = KeyCode.LeftArrow;
-    private KeyCode playerTwoRight = KeyCode.RightArrow;
-    private KeyCode playerTwoJump = KeyCode.UpArrow;
+    [SerializeField] private KeyCode playerTwoLeft = KeyCode.LeftArrow;
+    [SerializeField] private KeyCode playerTwoRight = KeyCode.RightArrow;
+    [SerializeField] private KeyCode playerTwoJump = KeyCode.UpArrow;
 
     public UnityEvent<Vector2> PlayerOneOnMove = new UnityEvent<Vector2>();
     public UnityEvent PlayerOneOnJump = new UnityEvent();
