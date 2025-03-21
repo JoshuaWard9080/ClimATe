813cf2fa3f0fb30a2be7278e08b8bd20e8a7394d
timmi <timmid999@gmail.com>
2025-03-21 10:58:23 -0700

    Implemented LevelCompleteScript, majority of the code has been reused from Main Menu, but this is just the start, much more needs to be changed and implemented.

---
+++ Assets/Scripts/LevelCompleteManager.cs
@@ -0,0 +1,112 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class LevelCompleteManager : MonoBehaviour
+{
+    [Header("UI Elements")]
+    [SerializeField] private Button nextLevelButton;
+    [SerializeField] private Button mainMenuButton;
+    [SerializeField] private Button quitGameButton;
+
+    //array to know which button the user is hovering over with the keyboard
+    private Button[] buttons;
+    private int selectedIndex = 0;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        buttons = new Button[] { nextLevelButton, mainMenuButton, quitGameButton };
+
+        if (nextLevelButton != null)
+        {
+            //not sure what the next level wil be yet
+            nextLevelButton.onClick.AddListener(NextLevel);
+        }
+
+        if (mainMenuButton != null)
+        {
+            mainMenuButton.onClick.AddListener(MainMenu);
+        }
+
+        if (quitGameButton != null)
+        {
+            quitGameButton.onClick.AddListener(QuitGame);
+        }
+
+        SelectButton(selectedIndex);
+
+        UpdatePointsUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            selectedIndex = (selectedIndex - 1 + buttons.Length) % buttons.Length;
+            SelectButton(selectedIndex);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            selectedIndex = (selectedIndex + 1 + buttons.Length) % buttons.Length;
+            SelectButton(selectedIndex);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return))
+        {
+            buttons[selectedIndex].onClick.Invoke();
+        }
+    }
+
+    private void SelectButton(int index)
+    {
+        EventSystem.current.SetSelectedGameObject(buttons[index].gameObject);
+    }
+
+    private void NextLevel()
+    {
+        Debug.Log("Loading next level...");
+        SceneManager.LoadScene("NextLevel"); //logic for next level needs to be implemented
+    }
+
+    private void MainMenu()
+    {
+        Debug.Log("Loading main menu...");
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    private void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
+
+
+
+    public void AddPoints(int amount)
+    {
+        //totalPoints += amount;
+        //Debug.Log("Points Updated: " + totalPoints);
+
+        UpdatePointsUI();
+    }
+
+    private void UpdatePointsUI()
+    {
+        //totalPointsText.text = "Total Points: " + totalPoints;
+    }
+}
