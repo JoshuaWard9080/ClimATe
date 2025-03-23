ada8991458b1e4c838412d0ab2cb0b78b70e06ac
Lucas Slunt <100328965+LucasSlunt@users.noreply.github.com>
2025-03-22 18:47:47 -0700

    Created basic floor and colour swap based on level number script

---
+++ Assets/Scripts/Level_Colour_Swap.cs
@@ -0,0 +1,57 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Level_Colour_Swap : MonoBehaviour
+{
+    public int level;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Color colour = Color.white;
+        if (level <= 2)
+        {
+            colour = Color.green;
+        }
+        else if (level <= 5)
+        {
+            colour = Color.gray;
+        }
+        else
+        {
+            colour= Color.blue;
+        }
+        findChildren(this.transform,colour);
+        //    int children = transform.childCount;
+        //    for (int i = 0; i < children; ++i)
+        //        print("For loop: " + transform.GetChild(i));
+
+    }
+
+    public void findChildren(Transform transform, Color colour)
+    {
+        int numOfChildren = transform.childCount;
+        if (numOfChildren > 0)
+        {
+            for (int i = 0; i < numOfChildren; ++i)
+            {
+                print("For loop: " + transform.GetChild(i));
+                findChildren(transform.GetChild(i),colour);
+
+            }
+        }
+        else
+        {
+            changeColour(transform, colour);
+        }
+    }
+
+    void changeColour(Transform transform,Color colour)
+    {
+        transform.GetComponent<Renderer>().material.color = colour;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
