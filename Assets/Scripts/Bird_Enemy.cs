daa114b4133ff8f8af36628eb71ecbb66307a934
Lucas Slunt <100328965+LucasSlunt@users.noreply.github.com>
2025-03-25 09:18:51 -0700

    Added warm behaviour to bird

--- Assets/Scripts/Bird_Enemy.cs
+++ Assets/Scripts/Bird_Enemy.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 
 public class Bird_Enemy : MonoBehaviour
 {
-    float moveSpeed = 0.003f;
-    Vector3 moveVector;
+    float moveSpeed = 0.010f;
+    public int moveTime = 100;
+    public Vector3 moveVector;
+    float boundX;
     [SerializeField] Boolean isHurt = false;
     public SpriteRenderer spriteRenderer;
     public Animator animator;
@@ -13,12 +15,13 @@ public class Bird_Enemy : MonoBehaviour
     void Start()
     {
         moveVector = new Vector3(1, 0, 0);
+        boundX = 9;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveBasedOnTemperature("warm");
     }
 
     void flipSprite()
@@ -28,17 +31,42 @@ public class Bird_Enemy : MonoBehaviour
 
     Vector3 chooseFlyDirection()
     {
-
-        return new Vector3(1, 1, 0);
+        
+        float theta = UnityEngine.Random.Range(0, 2*3.14159f);
+        float dirX = (float)Math.Cos(theta);
+        float dirY = (float)Math.Sin(theta);
+        
+        moveTime = (int)Math.Abs((UnityEngine.Random.Range(100, 800)*dirX));
+        return new Vector3(dirX, dirY, 0);
     }
 
     void moveBasedOnTemperature(String temp)
     {
+        if (temp == "warm") moveWarm();
+        else if (temp == "cold") moveCold();
+        else if (temp == "freezing") moveFreezing();
+        else
+        {
+            Debug.Log("Invalid temperature for bird");
 
+        }
     }
 
     void moveWarm()
     {
+        if (this.transform.position.x > boundX || this.transform.position.x < -boundX)
+        {
+            float oppositeDirX = -(moveVector.x);
+            moveVector = chooseFlyDirection();
+            moveVector.x = oppositeDirX;
+
+        }
+        if (moveTime <= 0)
+        {
+            moveVector = chooseFlyDirection();
+        }
+        moveTime--;
+        this.transform.position += moveVector * moveSpeed;
 
     }
 
