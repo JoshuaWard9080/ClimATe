59a978fd5456bd5d9b51d0655aee8660fb9a575d
JoshuaWard9080 <144284996+JoshuaWard9080@users.noreply.github.com>
2025-03-24 23:37:52 -0700

    Ported over changes
    Ported over the scripts and prefab from the broken branch to this branch

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,16 +1,49 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class InputManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Player One Keybinds")]
+    [SerializeField] private KeyCode playerOneLeft = KeyCode.A;
+    [SerializeField] private KeyCode playerOneRight = KeyCode.D;
+    [SerializeField] private KeyCode playerOneJump = KeyCode.W;
 
-    // Update is called once per frame
+    [Header("Player Two Keybinds")]
+    [SerializeField] private KeyCode playerTwoLeft = KeyCode.LeftArrow;
+    [SerializeField] private KeyCode playerTwoRight = KeyCode.RightArrow;
+    [SerializeField] private KeyCode playerTwoJump = KeyCode.UpArrow;
+
+    public UnityEvent<Vector2> playerOneOnMove = new UnityEvent<Vector2>();
+    public UnityEvent playerOneOnJumpEnd = new UnityEvent();
+    public UnityEvent playerOneOnJump = new UnityEvent();
+
+
     void Update()
     {
-        
+        Vector2 inputVector = new Vector2();
+
+        if (Input.GetKey(playerOneLeft))
+        {
+            inputVector += Vector2.left;
+        }
+
+        if (Input.GetKey(playerOneRight))
+        {
+            inputVector += Vector2.right;
+        }
+
+
+        playerOneOnMove?.Invoke(inputVector);
+
+        // there are two different jump events to handle a player holding vs tapping the jump button. allows for variable jump heights
+        if (Input.GetKeyDown(playerOneJump))
+        {
+            playerOneOnJump?.Invoke();
+        }
+        if (Input.GetKeyUp(playerOneJump))
+        {
+            playerOneOnJumpEnd?.Invoke();
+        }
+
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,106 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Rigidbody2D rb;
+    [SerializeField] private InputManager inputManager;
+
+    private float horizontal;
+    private bool isFacingRight = true;
+
+    [Header("Movement")]
+    [SerializeField] private float playerSpeed;
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float maxAirSpeed;
+
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float airMultiplier;
+
+    [Header("Ground Check")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+
+    // add the listeners for the events that InputManager throws and get the rigidbody
     void Start()
     {
-        
+        inputManager.playerOneOnMove.AddListener(MovePlayer);
+        inputManager.playerOneOnJump.AddListener(Jump);
+        inputManager.playerOneOnJumpEnd.AddListener(JumpEnd);
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    // update ensures that the player doesn't go above the speed limit and handles flipping the character for sprite stuff
     void Update()
     {
-        
+        SpeedControl();
+        Flip();
+    }
+
+    // adds force to the rigidbody to move the character. Applies a different speed if the character is in the air
+    public void MovePlayer(Vector2 input)
+    {
+        if (IsGrounded())
+            rb.AddForce(input.normalized * playerSpeed, ForceMode2D.Force);
+
+        else if (!IsGrounded())
+            rb.AddForce(input.normalized * playerSpeed * airMultiplier, ForceMode2D.Force);
+
+        horizontal = input.normalized.x;
+    }
+
+    // handles flipping the character
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+
+    // checks if the player is on the ground by using the OverlapCircle method. the groundLayer should be the ground tag in the game
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    // handles jumps. makes the y component equal to the jump force
+    public void Jump()
+    {
+        if (IsGrounded())
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+    // handles the end of the jump. This makes the player slow their upward movement and end the jump early if they tap vs hold the jump key
+    public void JumpEnd()
+    {
+        if (rb.linearVelocity.y > 0f)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+        }
+    }
+
+    // enforces the speed limit. Its a school zone after all
+    // has a different speed limit for when in the air vs on the ground, controlled by playerSpeed and maxAirSpeed
+    public void SpeedControl()
+    {
+        Vector2 flatVelocity = new Vector2(rb.linearVelocity.x, 0f);
+
+        if (flatVelocity.magnitude > maxSpeed)
+        {
+            Vector2 limitedVelocity = flatVelocity.normalized * maxSpeed;
+            rb.linearVelocity = new Vector2(limitedVelocity.x, rb.linearVelocity.y);
+        }
+
+        if (!IsGrounded() && flatVelocity.magnitude > maxAirSpeed)
+        {
+            Vector2 limitedVelocity = flatVelocity.normalized * maxAirSpeed;
+            rb.linearVelocity = new Vector2(limitedVelocity.x, rb.linearVelocity.y);
+        }
     }
 }
