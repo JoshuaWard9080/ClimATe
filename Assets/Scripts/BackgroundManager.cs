8f88677862764f633b32d3ba354e0c106c4862bf
Lucas Slunt <100328965+LucasSlunt@users.noreply.github.com>
2025-03-30 15:52:00 -0700

    Clouds move

--- Assets/Scripts/BackgroundManager.cs
+++ Assets/Scripts/BackgroundManager.cs
@@ -8,10 +8,17 @@ public class BackgroundManager : MonoBehaviour
     [SerializeField] private Transform Mountains;
     [SerializeField] private Transform Midground;
     [SerializeField] private Transform Foreground;
+    [SerializeField] private float speed;
+    private Transform CloudsFirst;
+    private Transform CloudsSecond;
+    private Transform IntitialCloudPos;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         transform.position = new Vector3(playerCamera.transform.position.x, playerCamera.transform.position.y, 0);
+        CloudsFirst = Clouds.GetChild(0);
+        CloudsSecond = Clouds.GetChild(1);
+        IntitialCloudPos = CloudsFirst.transform;
     }
 
     // Update is called once per frame
@@ -22,12 +29,22 @@ public class BackgroundManager : MonoBehaviour
         Clouds.position -= (deltaPosition / 40);
         Mountains.position -= (deltaPosition /16);
         Midground.position -= (deltaPosition/8);
-        Foreground.position -= (deltaPosition/5); 
+        Foreground.position -= (deltaPosition/5);
+        moveClouds();
     }
 
     Vector3 calculateChangeInCameraPosition()
     {
         Vector3 newPosition = new Vector3(playerCamera.transform.position.x, playerCamera.transform.position.y, 0);
         return newPosition - transform.position;
+    }
+
+    void moveClouds()
+    {
+        float moveAmount = speed * Time.deltaTime;
+
+        CloudsFirst.transform.position -= new Vector3(moveAmount, 0,0);
+        CloudsSecond.transform.position -= new Vector3(moveAmount, 0, 0);
+        
     }
 }
