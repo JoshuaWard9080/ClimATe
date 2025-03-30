35bd0f8678b50e7f5efe314c6098d81c2a4af1b6
Lucas Slunt <100328965+LucasSlunt@users.noreply.github.com>
2025-03-30 15:30:34 -0700

    Background parallax

--- Assets/Scripts/BackgroundManager.cs
+++ Assets/Scripts/BackgroundManager.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class BackgroundManager : MonoBehaviour
 {
-    [SerializeField] private Camera camera;
+    [SerializeField] private Camera playerCamera;
     [SerializeField] private Transform Sky;
     [SerializeField] private Transform Clouds;
     [SerializeField] private Transform Mountains;
@@ -11,12 +11,23 @@ public class BackgroundManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        transform.position = new Vector3(playerCamera.transform.position.x, playerCamera.transform.position.y, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform = camera.transform;
+        Vector3 deltaPosition = calculateChangeInCameraPosition();
+        transform.position += deltaPosition;
+        Clouds.position -= (deltaPosition / 40);
+        Mountains.position -= (deltaPosition /16);
+        Midground.position -= (deltaPosition/8);
+        Foreground.position -= (deltaPosition/5); 
+    }
+
+    Vector3 calculateChangeInCameraPosition()
+    {
+        Vector3 newPosition = new Vector3(playerCamera.transform.position.x, playerCamera.transform.position.y, 0);
+        return newPosition - transform.position;
     }
 }
