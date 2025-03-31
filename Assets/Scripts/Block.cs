ffa56ee7a7baaf58169a85e312c46710ddb95dc1
timmi <timmid999@gmail.com>
2025-03-31 13:32:05 -0700

    Block audio when hit by player

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    [Header("Audio")]
+    [SerializeField] private AudioSource blockDestroyedAudio;
 
     private float xBound = 0.4f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -17,10 +19,20 @@ public class Block : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Player" 
+        if (collision.gameObject.tag == "Player"
             && collision.gameObject.transform.position.y < this.transform.position.y
             && collision.gameObject.transform.position.x - this.transform.position.x < xBound
             && this.transform.position.x - collision.gameObject.transform.position.x < xBound){
+
+            if (blockDestroyedAudio != null && blockDestroyedAudio.clip != null)
+            {
+                AudioSource.PlayClipAtPoint(blockDestroyedAudio.clip, transform.position);
+            }
+            else
+            {
+                Debug.Log("Audio is null for destroying a block");
+            }
+
             onHitByPlayer();
         }
     }
