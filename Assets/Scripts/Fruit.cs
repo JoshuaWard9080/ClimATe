bd06eac1ad1405570f34f33b57b5f123fcba7c90
Lucas Slunt <100328965+LucasSlunt@users.noreply.github.com>
2025-03-30 21:41:10 -0700

    added sprites, fruit script

---
+++ Assets/Scripts/Fruit.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Fruit : MonoBehaviour
+{
+    [SerializeField] public Sprite fruitImg1;
+    [SerializeField] public Sprite fruitImg2;
+    [SerializeField] public Sprite fruitImg3;
+    [SerializeField] public Sprite fruitImg4;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
