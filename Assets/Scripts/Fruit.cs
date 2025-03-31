7d66995f242da2cf9c1e6f19bdfb126ce4f69555
Lucas Slunt <100328965+LucasSlunt@users.noreply.github.com>
2025-03-30 21:49:41 -0700

    fruit randomly selects a sprite to use

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -4,19 +4,13 @@ using UnityEngine.UI;
 
 public class Fruit : MonoBehaviour
 {
-    [SerializeField] public Sprite fruitImg1;
-    [SerializeField] public Sprite fruitImg2;
-    [SerializeField] public Sprite fruitImg3;
-    [SerializeField] public Sprite fruitImg4;
+    [SerializeField] public Sprite[] sprites;
+    [SerializeField] public SpriteRenderer spriteRenderer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
+        spriteRenderer.sprite = sprites[Random.Range(0,sprites.Length)];
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
