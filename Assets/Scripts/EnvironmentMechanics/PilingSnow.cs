b47cb9e688f2f3b0b856d47dc92a0ac3a74fc6ce
JoshuaWard9080 <144284996+JoshuaWard9080@users.noreply.github.com>
2025-03-31 12:02:03 -0700

    Updated Piling Snow
    Updated Piling Snow to listen for the temperature change and make the corresponding changes

--- Assets/Scripts/EnvironmentMechanics/PilingSnow.cs
+++ Assets/Scripts/EnvironmentMechanics/PilingSnow.cs
@@ -3,7 +3,19 @@ using UnityEngine;
 public class PilingSnow : MonoBehaviour
 {
     [SerializeField] private float snowSpeed = 1f;
+    [SerializeField] private TemperatureManager temperatureManager;
     private bool isRising = false;
+
+    private float coldSnowSpeed = 1f;
+    private float freezingSnowSpeed = 2f;
+    private float warmSnowSpeed = 0.5f;
+
+    private void Start()
+    {
+        temperatureManager.OnTempChangeToCold.AddListener(SetCold);
+        temperatureManager.OnTempChangeToFreezing.AddListener(SetFreezing);
+        temperatureManager.OnTempChangeToWarm.AddListener(SetWarm);
+    }
 
 
     void Update()
@@ -32,5 +44,20 @@ public class PilingSnow : MonoBehaviour
     {
         snowSpeed = newSnowSpeed;
         Debug.Log("Snow piling speed set to: " + newSnowSpeed);
+    }
+
+    public void SetFreezing()
+    {
+        snowSpeed = freezingSnowSpeed;
+    }
+
+    public void SetCold()
+    {
+        snowSpeed = coldSnowSpeed;
+    }
+
+    public void SetWarm()
+    {
+        snowSpeed = warmSnowSpeed;
     }
 }
