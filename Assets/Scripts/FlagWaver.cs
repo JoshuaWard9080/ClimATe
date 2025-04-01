c4a290a29c353e722585dbe9a6829b4f9ef172de
timmi <timmid999@gmail.com>
2025-03-31 22:50:38 -0700

    Created prefab and added flags to each level

---
+++ Assets/Scripts/FlagWaver.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class FlagWaver : MonoBehaviour
+{
+    [Header("Waving Animation")]
+    public float frequency = 2f;
+    public float amplitude = 0.1f;
+    private Vector3 originalScale;
+
+    [Header("Color Animation")]
+    public Color colorA = Color.green;
+    public Color colorB = Color.white;
+    public float colorSpeed = 2f;
+
+    private SpriteRenderer spriteRenderer;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        originalScale = transform.localScale;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float wave = Mathf.Sin(Time.time * frequency) * amplitude;
+        transform.localScale = new Vector3(originalScale.x + wave, originalScale.y, originalScale.z); //sine wave equation, hopefully makes the flag wave properly
+
+        float lerpVal = Mathf.Sin(Time.time * colorSpeed) * 0.5f + 0.5f;
+        spriteRenderer.color = Color.Lerp(colorA, colorB, lerpVal);
+    }
+}
