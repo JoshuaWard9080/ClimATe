cf9c093cd9b596413de7b97ad4f740a05601db7d
Lucas Slunt <100328965+LucasSlunt@users.noreply.github.com>
2025-03-31 19:14:21 -0700

    Updated bird damage logic

--- Assets/Scripts/Bird_Enemy.cs
+++ Assets/Scripts/Bird_Enemy.cs
@@ -242,14 +242,15 @@ public class Bird_Enemy : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player"){
-            if (collision.gameObject.transform.position.y > this.transform.position.y)
+            if (collision.gameObject.transform.position.y > this.transform.position.y + 0.3)
             {
                 moveVector = new Vector3(0, -1, 0);
                 isHurt = true;
                 animator.SetBool("isHurt", true);
                 gameObject.transform.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+                Physics2D.IgnoreCollision(collision.collider, GetComponent<Collider2D>());
 
-            if (playerHitsBirdAudio != null)
+                if (playerHitsBirdAudio != null)
             {
                 playerHitsBirdAudio.Play(); //make sure loop does NOT equal true in inspector
             }
