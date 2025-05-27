8b6cf37990629b8531d45947e01dcc30903391f0
Lucas Slunt <100328965+LucasSlunt@users.noreply.github.com>
2025-05-26 21:02:41 -0700

    yetis avoid pits properly

--- Assets/Scripts/FuzzyEnemy.cs
+++ Assets/Scripts/FuzzyEnemy.cs
@@ -96,12 +96,11 @@ public class FuzzyEnemy : MonoBehaviour
     {
         float direction = moveVector.x;
         Vector2 raycastStart =
-            new Vector2(this.transform.position.x+(direction*0.3f*(this.transform.localScale.x+0.4f)), this.transform.position.y-(0.5f*this.transform.localScale.y));
+            new Vector2(this.transform.position.x+(direction*(this.transform.localScale.x*0.5f)), this.transform.position.y-(0.5f*this.transform.localScale.y));
         Vector2 raycastDirection = transform.TransformDirection(Vector2.down);
         float maxDistance = 0.2f;
         Debug.DrawRay(raycastStart, raycastDirection* maxDistance);
         RaycastHit2D hit = Physics2D.Raycast(raycastStart, raycastDirection, maxDistance);
-        //if (hit.collider.gameObject.tag == "Block") return false;
         if (hit.collider == null) return true;
         return false;
     }
