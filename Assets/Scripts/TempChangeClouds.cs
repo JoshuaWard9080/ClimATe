d6a0821d5c5496b82b36efd008f1286be84813b5
Lucas Slunt <100328965+LucasSlunt@users.noreply.github.com>
2025-06-02 18:49:47 -0700

    clouds move up and down, and with player

--- Assets/Scripts/TempChangeClouds.cs
+++ Assets/Scripts/TempChangeClouds.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 public class TempChangeClouds : MonoBehaviour
 {
     [SerializeField] TemperatureManager temperatureManager;
-    float moveSpeed = 0.003f;
+    [SerializeField] Transform player;
+    float moveSpeed = -0.01f;
     Color warmColour = new Color(81 / 100f, 72 / 100f, 60 / 100f);
     Color coldColour = new Color(180 / 255f, 226 / 255f, 255 / 255f);
-    Color freezingColour = new Color(8 / 100f, 16 / 100f, 100 / 100f);
+    Color freezingColour = new Color(16 / 100f, 42 / 100f, 100 / 100f);
     Boolean hasTempChanged = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,13 +16,24 @@ public class TempChangeClouds : MonoBehaviour
         temperatureManager.OnTempChangeToWarm.AddListener(tempChangeToWarm);
         temperatureManager.OnTempChangeToCold.AddListener(tempChangeToCold);
         temperatureManager.OnTempChangeToFreezing.AddListener(tempChangeToFreezing);
-
+        hasTempChanged = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         if (!hasTempChanged) return;
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            float thetaMultiplier = 1f+(27 % (i + 1)) / 23; 
+            transform.GetChild(i).transform.position = new Vector3(
+                transform.GetChild(i).transform.position.x +moveSpeed,
+                player.position.y+ thetaMultiplier+((float)(thetaMultiplier*Math.Sin(transform.GetChild(i).transform.position.x))),
+                0);
+
+
+        }
 
 
     }
