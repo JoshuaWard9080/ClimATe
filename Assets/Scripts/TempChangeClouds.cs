3c063800f1aeea859a451806a551eba06a776840
Lucas Slunt <100328965+LucasSlunt@users.noreply.github.com>
2025-06-02 19:08:18 -0700

    clouds reset pos on temp change trigger

--- Assets/Scripts/TempChangeClouds.cs
+++ Assets/Scripts/TempChangeClouds.cs
@@ -5,7 +5,7 @@ public class TempChangeClouds : MonoBehaviour
 {
     [SerializeField] TemperatureManager temperatureManager;
     [SerializeField] Transform player;
-    float moveSpeed = -0.01f;
+    float moveSpeed = -0.013f;
     Color warmColour = new Color(81 / 100f, 72 / 100f, 60 / 100f);
     Color coldColour = new Color(180 / 255f, 226 / 255f, 255 / 255f);
     Color freezingColour = new Color(16 / 100f, 42 / 100f, 100 / 100f);
@@ -26,10 +26,10 @@ public class TempChangeClouds : MonoBehaviour
 
         for (int i = 0; i < transform.childCount; i++)
         {
-            float thetaMultiplier = 1f+(27 % (i + 1)) / 23; 
+            float thetaMultiplier = 0.8f+(27 % (i + 1)) / 27; 
             transform.GetChild(i).transform.position = new Vector3(
                 transform.GetChild(i).transform.position.x +moveSpeed,
-                player.position.y+ thetaMultiplier+((float)(thetaMultiplier*Math.Sin(transform.GetChild(i).transform.position.x))),
+                player.position.y+ 2*thetaMultiplier+((float)(thetaMultiplier*((1/2.0)*Math.Sin((1 / 2.0) * transform.GetChild(i).transform.position.x)))),
                 0);
 
 
@@ -41,19 +41,39 @@ public class TempChangeClouds : MonoBehaviour
     void tempChangeToWarm()
     {
         changeCloudColour(warmColour);
+        resetCloudPosition();
         hasTempChanged = true;
     }
 
     void tempChangeToCold()
     {
         changeCloudColour(coldColour);
+        resetCloudPosition();
         hasTempChanged = true;
     }
 
     void tempChangeToFreezing()
     {
         changeCloudColour(freezingColour);
+        resetCloudPosition();
         hasTempChanged = true;
+    }
+
+    void resetCloudPosition()
+    {
+       float  distanceTravelled = -transform.GetChild(0).transform.position.x-11f;
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            
+            transform.GetChild(i).transform.position += new Vector3(
+                distanceTravelled,
+                0,  
+                0);
+
+
+        }
+
     }
 
     void changeCloudColour(Color colour)
